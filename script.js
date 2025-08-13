b41bc51535072f28209e65f8902534885d1ae3cd
an0u5h3h <hussainanousheh@gmail.com>
2025-08-13 19:54:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let show = () => {
     let name = document.getElementById("name").value;
     document.getElementById("message1").innerHTML="Wish you a very Happy New Year to "+name;
-    document.getElementById("message2").src="./images/2023.gif";
+    document.getElementById("image").src="./assets/2026.gif";
     document.getElementById("message3").innerHTML="New year, new hopes and newer beginnings- all of us ardently wait for the clock to strike 12 and usher in the new year. It's such a celebratory, positive time which keeps everyone in good spirits!";
     }
-document.getElementById("button1").addEventListener("click", show);
+// document.getElementById("button1").addEventListener("click", show); 
+document.addEventListener("DOMContentLoaded", function(){
+const btn=document.getElementById("button1");
+btn.addEventListener("click", show)
+})
+console.log("script loaded")
